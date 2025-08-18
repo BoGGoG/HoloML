f36978e5ab339827eb8451a3efe8a6e53ca7c4a1
Marco Knipfer <marco.knipfer@me.com>
2025-08-18 17:13:32 +0200

    Model forward pass working.
    Had forgotten that there is no batching

--- BTZ/BTZ_train.py
+++ BTZ/BTZ_train.py
@@ -32,6 +32,8 @@ from BTZ import (
     l_integral_NN,
     _h,
     _f,
+    f_true,
+    h_true,
     # get_event_horizon,
     # get_thermal_entropy,
 )
@@ -55,8 +57,6 @@ def power_law_fit(data):
     idxs = np.where((zstar > 0.1) & (zstar < 0.99))[0]
     S = S[idxs]
     l = l[idxs]
-    print(f"{S=}")
-    print(f"{l=}")
 
     popt, pcov = curve_fit(f_for_fit, l, S, maxfev=100000)
     return popt, pcov
@@ -72,20 +72,64 @@ if __name__ == "__main__":
     set_up_backend("torch")
 
     # Define the parameters for the BTZ black hole
-    c, v, beta = 24 * np.pi, 2 * np.pi, 1.0
+    c, v, beta = 24 * np.pi, 1.0, 2 * np.pi
+    N_zstar_points = 200
 
     # load data
     dir = Path("data")
     path = dir / "data_BTZ.pkl"
     print(f"Loading data from {path}")
     data_loaded = pickle.load(open(path, "rb"))
-    S_true = data_loaded[(c, beta, v)]["SFinite"]
-    l_true = data_loaded[(c, beta, v)]["l"]
-    zstar_true = data_loaded[(c, beta, v)]["zstar"]
+    S_true = data_loaded[(c, v, beta)]["SFinite"]
+    l_true = data_loaded[(c, v, beta)]["l"]
+    zstar_true = data_loaded[(c, v, beta)]["zstar"]
     s_thermal_true = torch.tensor(
-        data_loaded[(c, beta, v)]["s_thermal"], dtype=torch.float64
+        data_loaded[(c, v, beta)]["s_thermal"], dtype=torch.float64
     )
 
-    popt = interpolate_S_l(data_loaded[(c, beta, v)])
-    print(f"Fitted parameters for {(c, beta, v)}:")
+    popt = interpolate_S_l(data_loaded[(c, v, beta)])
+    print(f"Fitted parameters for (c, v, beta) = {(c, v, beta)}:")
     print(popt)
+
+    zstar_list = torch.tensor(
+        np.linspace(0.10, 0.999, N_zstar_points),
+        requires_grad=False,
+        dtype=torch.float64,
+    )
+
+    model = BTZ_NN(input_dim=1, output_dim=1, hidden_layers=[32, 32])
+    model.to(torch.float64)  # Ensure model uses float64 precision
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    scheduler = optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=[50, 120, 200, 300, 500, 700, 900], gamma=0.5
+    )
+
+    def h_helper(z, Q):
+        return _h(model, z)
+
+    epochs = 2
+    loss_hist = []
+    s_thermal_loss_hist = []
+    S_loss_hist = []
+    lr_hist = []
+
+    print(f"{zstar_list.dtype=}")
+    asdf = model(zstar_list[0:1])
+    print(f"{asdf.shape=}")
+
+    # for epoch in range(epochs):
+    #     zstar_list_noise = (
+    #         zstar_list + torch.randn_like(zstar_list) * 0.005
+    #     )  # add some noise
+    #     zstar_list_noise = torch.clamp(
+    #         zstar_list_noise, min=0.10, max=0.9999
+    #     )  # avoid numerical issues
+    #     if epoch == epochs:
+    #         N_zstar_points = 1000
+    #         zstar_list = torch.tensor(
+    #             np.linspace(0.10, 0.999, N_zstar_points),
+    #             requires_grad=False,
+    #             dtype=torch.float64,
+    #         )
+    #     S_pred = S_integral_NN(model, zstar_list_noise, N_GL=12)
+    #     l_pred = l_integral_NN(model, zstar_list_noise, N_GL=12)
